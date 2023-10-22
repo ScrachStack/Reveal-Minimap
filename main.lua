0f0b782a5615302cef94479d2dc9988e900f0a7d
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-10-22 14:54:03 -0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,4 @@
-CreateThread(function()
-    Citizen.Wait(0)
-    for i = 0, 25 do
+for i = 0, 25 do
         Citizen.InvokeNative(0x4B8F743A4A6D2FF8, i)  -- REVEAL_MINIMAP_SEGMENT
-    end
-end)
+
+end
